cf4af8b8945cddbae8a048860f1bf5d39f857265
johnlee1 <telljlee@gmail.com>
2016-07-18 16:51:19 -0400

    added requirements.txt and used env var in selenium-test.py

--- selenium-test.py
+++ selenium-test.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-  
+
+
+SAUCE_USERNAME = os.environ.get('SAUCE_USERNAME')
+SAUCE_ACCESS_KEY = os.environ.get('SAUCE_ACCESS_KEY')
+print SAUCE_USERNAME
 
 # This is the only code you need to edit in your existing scripts.
 # The command_executor tells the test to run on Sauce, while the desired_capabilities
@@ -12,11 +16,10 @@ desired_cap = {
     'version': "31",
 }
 driver = webdriver.Remote(
-   command_executor='http://johnlee1:d26a6ba1-c5fd-4086-af56-6422d9e6e405@ondemand.saucelabs.com:80/wd/hub',
-   #command_executor='http://YOUR_SAUCE_USERNAME:YOUR_SAUCE_ACCESS_KEY@ondemand.saucelabs.com:80/wd/hub',
+   command_executor='http://'+SAUCE_USERNAME':'+SAUCE_ACCESS_KEY'@ondemand.saucelabs.com:80/wd/hub',
    desired_capabilities=desired_cap)
   
-
+  
 # This is your test logic. You can add multiple tests here.
 driver.implicitly_wait(10)
 driver.get("http://127.0.0.1:5000/")
